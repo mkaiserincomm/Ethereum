1333c646418700b7ef1a061e8cb32baea1321832
mkaiserincomm <mkaiser@incomm.com>
2020-12-29 22:04:54 +0000

    Mostly working

--- entity.js
+++
@@ -1,9 +0,0 @@
-// Wrapper class for Ethereum REST API request body
-module.exports = class entity {
-    constructor(method, params) {
-        this.jsonrpc = '2.0';
-        this.method = method;
-        this.params = params;
-        this.id = 1;
-    }
-}
