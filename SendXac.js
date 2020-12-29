de215a37d39a7bb066eba0a58b0d6381263ca3c0
mkaiserincomm <mkaiser@incomm.com>
2020-12-29 23:13:46 +0000

    Working Transaction

--- SendXac.js
+++ SendXac.js
@@ -3,6 +3,7 @@ const solc          = require("solc");
 const Tx            = require('ethereumjs-tx').Transaction;
 const keythereum    = require("keythereum");
 const Common        = require('ethereumjs-common').default;
+const Utils         = require('web3-utils')
 
 // Connect to the network
 const web3          = new Web3("ws://localhost:3304");
@@ -13,6 +14,7 @@ const datadir       = "/home/mkaiser/.ethereum/04";
 const password      = "P@ssw0rd";
 const keyObject     = keythereum.importFromFile(account, datadir);
 const privateKey    = keythereum.recover(password, keyObject);
+const value         = 14000000000000000;
 
 // Use our private chain
 const customCommon = Common.forCustomChain('mainnet', { name: 'my-private-blockchain', networkId: 1, chainId:0x3ad }, 'petersburg' );
@@ -25,14 +27,13 @@ const gasLimitHex = web3.utils.toHex(3000000);
 async function Xac()
 {        
     let txParams = {
-        from: account,
-        to: "0xcdf97451671304407b362c2a354bfc71b287a969",
-        gasLimit: gasLimitHex,
-        gasPrice: gasPriceHex,
-        nonce: "0x" + (await web3.eth.getTransactionCount('0x18ecbbb365a076c30793412503ba21a01e637922')).toString(16),
-        value: "0x10000000000"
-    }    
-    
+        from:       account,
+        to:         "0xcdf97451671304407b362c2a354bfc71b287a969",
+        gasLimit:   gasLimitHex,
+        gasPrice:   gasPriceHex,
+        nonce:      "0x" + (await web3.eth.getTransactionCount('0x18ecbbb365a076c30793412503ba21a01e637922')).toString(16),
+        value:      Utils.numberToHex(value)    } ;
+        
     let tx = new Tx(txParams, { common: customCommon });
     tx.sign(privateKey);
     let stx = tx.serialize();
